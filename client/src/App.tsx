f5ee502823fa14539187a7c07bd6a5fe19c1ed81
mbrndt <mareike.brandt@hotmail.de>
2023-05-02 17:38:47 +0900

    feat: added folder structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,29 @@
+import { Box, CssBaseline } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useMemo } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { themeSettings } from "./theme";
+
 function App() {
-	return <div className="app"></div>;
+	const theme = useMemo(() => createTheme(themeSettings), []);
+	return (
+		<div className="app">
+			<BrowserRouter>
+				<ThemeProvider theme={theme}>
+					<CssBaseline />
+					<Box width="100%" height="100vh" padding="1rem 2rem 4rem 2rem">
+						<Routes>
+							<Route path="/" element={<div>dashboard page</div>} />
+							<Route
+								path="/predictions"
+								element={<div>predictions page</div>}
+							/>
+						</Routes>
+					</Box>
+				</ThemeProvider>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
